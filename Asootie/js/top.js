6aa7e0bede30a6a1b9643b906a03d5703f0c277d
Myon　 <2201195@s.asojuku.ac.jp>
2024-05-08 10:20:58 +0900

    upload

---
+++ Asootie/js/top.js
@@ -0,0 +1,5 @@
+// ページが読み込まれたときの処理
+window.onload = function() {
+    // body要素にフェードアウトのクラスを追加して表示する
+    document.body.classList.add('fade-in');
+};
---
+++ Asootie/top.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asou知恵袋</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/fade.css">  -->
+</head>
+
+<body>
+    <div class="header-top"></div>
+    <header>
+
+        <div class="search">
+            <div class="logo">
+                <img src="img/asootie.png" alt="ロゴ">
+            </div>
+
+            <form action="#">
+                <input type="text" id="searchInput" placeholder="Q&Aを探す">
+                <button type="submit">検索</button>
+            </form>
+        </div>
+        <script src="js/top.js"></script> <!-- JavaScriptファイルの読み込み -->
+</body>
+
+</html>
