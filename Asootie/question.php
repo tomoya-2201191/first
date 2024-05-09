881c7ef2d928c1db1ee294ff5019dd92d96305fd
tomoya-2201191 <148730789+tomoya-2201191@users.noreply.github.com>
2024-05-09 12:39:35 +0900

    update

--- Asootie/question.php
+++ Asootie/question.php
@@ -20,20 +20,61 @@
 </head>
 
 <body>
-    <div class="header-top"></div>
-    <header>
+<div class="header-top"></div>
 
-        <div class="search">
-            <div class="logo">
-                <img src="img/asootie.png" alt="ロゴ">
-            </div>
+<header>
 
-            <form action="#">
-                <input type="text" id="searchInput" placeholder="Q&Aを探す">
-                <button type="submit">検索</button>
-            </form>
+    <div class="search">
+        <div class="logo">
+            <img src="img/asootie.png" alt="ロゴ">
         </div>
-        <script src="js/top.js"></script> <!-- JavaScriptファイルの読み込み -->
+        <form action="#">
+            <input type="text" id="searchInput" placeholder="Q&Aを探す">
+            <button type="submit">検索</button>
+        </form>
+        <div class="icon">
+            <img src="img/icon.png" height="80" weight="100">
+        </div>
+    </div>
+</header>
+<div class="question">
+<a class="questionn" href="question.php" >aaaaaaaaaa</a>
+</div>
+<div class="a1"></div>
+
+<div class="flex">
+
+<div class="aaa">
+    <?php
+    $pdo= new PDO($connect,USER,PASS);
+    $sql=$pdo->query('select * from user');
+    $row = $sql->fetch(PDO::FETCH_ASSOC);
+    echo '<div class="q_user">';
+    echo '<img src="img/icon.png" height="80" weight="100" class="icon">';
+    echo "<p>",$row['name'],"　さん","</p>";
+    echo '</div>';
+    ?>
+</div>
+
+<div class="bbb">
+    <?php
+    echo '<div class="category">';
+    $sql=$pdo->query('select * from category');
+    echo '<br>','　カテゴリ一覧';
+    echo '<hr>';
+    echo '<ul>';
+    foreach ($sql as $row) {
+        $id=$row['category_id'];
+        echo '<li><a href="#?id=', $id, '">',$row['category_name'],"</li>";
+        echo '<br>';
+    }
+    echo "</ul>";
+    echo '<hr>';
+    ?>
+</div>
+
+</div>
+    <script src="js/top.js"></script> <!-- JavaScriptファイルの読み込み -->
 </body>
 
 </html>
