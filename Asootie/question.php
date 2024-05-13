ab309674817277a04a67b49d0f51289f494edc2b
Myon　 <2201195@s.asojuku.ac.jp>
2024-05-13 12:43:53 +0900

    up

--- Asootie/question.php
+++ Asootie/question.php
@@ -1,27 +1,63 @@
 <?php
-session_start();;
-require 'db-connect.php';
-require 'header.php';
+    const SERVER = 'mysql302.phy.lolipop.lan';
+    const DBNAME = 'LAA1516825-aso';
+    const USER = 'LAA1516825';
+    const PASS = 'aso1234';
 
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
 ?>
 
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asoo!知恵袋</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/fade.css">  -->
+</head>
+
+<body>
+<div class="header-top"></div>
+
+<header>
+
+    <div class="search">
+        <div class="logo">
+            <img src="img/asootie.png" alt="ロゴ">
+        </div>
+        <form action="#">
+            <input type="text" id="searchInput" placeholder="Q&Aを探す">
+            <button type="submit">検索</button>
+        </form>
+        <div class="icon">
+            <img src="img/icon.png" height="80" weight="100">
+        </div>
+    </div>
+</header>
+<div class="question">
+<a class="questionn" href="question.php" >aaaaaaaaaa</a>
+</div>
 <div class="a1"></div>
 
 <div class="flex">
 
 <div class="aaa">
     <?php
+    $pdo= new PDO($connect,USER,PASS);
     $sql=$pdo->query('select * from user');
     $row = $sql->fetch(PDO::FETCH_ASSOC);
     echo '<div class="q_user">';
-    echo '<img src="img/icon.png" height="80" weight="100">';
-    echo "<p>",$row['name'],"　さん","<br>";
-    echo "aaaa","","</p>";
+    echo '<img src="img/icon.png" height="80" weight="100" class="icon">';
+    echo "<p>",$row['name'],"　さん","</p>";
     echo '</div>';
     ?>
 </div>
 
-<div class="bbb">
+<div class="right">
     <?php
     echo '<div class="category">';
     $sql=$pdo->query('select * from category');
@@ -39,6 +75,7 @@ require 'header.php';
 </div>
 
 </div>
-<?php
-require 'footer.php';
-?>
+    <script src="js/top.js"></script> <!-- JavaScriptファイルの読み込み -->
+</body>
+
+</html>
