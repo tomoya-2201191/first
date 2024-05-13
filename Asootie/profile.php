70c3121e2b66f227100768dd71525a63f147f420
kyo0622 <148731274+kyo0622@users.noreply.github.com>
2024-05-13 11:53:01 +0900

    Create profile.php

---
+++ Asootie/profile.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asoo!知恵袋</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/profile.css">
+    <!-- <link rel="stylesheet" href="css/fade.css">  -->
+</head>
+
+<body>
+    <div class="header-top"></div>
+
+    <header>
+
+        <div class="search">
+            <div class="logo">
+                <img src="img/asootie.png" alt="ロゴ">
+            </div>
+            <form action="#">
+                <input type="text" id="searchInput" placeholder="Q&Aを探す">
+                <button type="submit">検索</button>
+            </form>
+            <div class="icon">
+                <img src="img/icon.png" height="80" weight="100">
+            </div>
+        </div>
+    </header>
+    <div class="question">
+    <a class="questionn" href="question.php" >aaaaaaaaaa</a>
+    </div>
+    
+
+    <div class="flex">
+
+    <div class="aaa"></div>
+    
+
+    </div>
+        <script src="js/profile.js"></script> <!-- JavaScriptファイルの読み込み -->
+</body>
+
+</html>
