77c8dc133b8d95b226e1ab07b280ba6d8eacdd1a
hide11081 <148840580+hide11081@users.noreply.github.com>
2024-05-16 12:09:35 +0900

    Create kaitou.php

---
+++ Asootie/kaitou.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="header-top"></div>
+
+<header>
+
+
+    <div class="logo">
+        <img src="img/asootie.png" alt="ロゴ">
+    </div>
+
+    <div class="search_box">
+
+        <form method="get" action="#" class="search">
+              <div class="searchForm">
+                <input type="text" class="searchForm-input" placeholder="Q&Aを探す">
+                <button type="submit" class="searchForm-submit"></button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
