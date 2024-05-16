6cfce1a86b51bbc5f141bb07f29f89fc516432d4
hide11081 <148840580+hide11081@users.noreply.github.com>
2024-05-16 12:11:51 +0900

    Update kaitou.php

--- Asootie/kaitou.php
+++ Asootie/kaitou.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<div class="header-top"></div>
+<?php
+session_start();;
+require 'db-connect.php';
+require 'header.php';
 
-<header>
-
-
-    <div class="logo">
-        <img src="img/asootie.png" alt="ロゴ">
-    </div>
-
-    <div class="search_box">
-
-        <form method="get" action="#" class="search">
-              <div class="searchForm">
-                <input type="text" class="searchForm-input" placeholder="Q&Aを探す">
-                <button type="submit" class="searchForm-submit"></button>
-            </div>
-        </form>
-    </div>
-</body>
-</html>
+?>
