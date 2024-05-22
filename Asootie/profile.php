a9bf5f4d6a05061a00efe8609391abb4dca27d4e
kyo0622 <148731274+kyo0622@users.noreply.github.com>
2024-05-22 09:48:22 +0900

    up

--- Asootie/profile.php
+++ Asootie/profile.php
@@ -1,45 +1,36 @@
-<!DOCTYPE html>
-<html lang="ja">
+<?php
+session_start();;
+require 'db-connect.php';
+require 'header.php';
 
-<head>
-    <meta charset="UTF-8">
+?>
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Asoo!知恵袋</title>
-    <link rel="stylesheet" href="css/reset.css">
     <link rel="stylesheet" href="css/profile.css">
-    <!-- <link rel="stylesheet" href="css/fade.css">  -->
-</head>
+<div class="a1"></div>
 
-<body>
-    <div class="header-top"></div>
+<div class="flex">
 
-    <header>
-
-        <div class="search">
-            <div class="logo">
-                <img src="img/asootie.png" alt="ロゴ">
-            </div>
-            <form action="#">
-                <input type="text" id="searchInput" placeholder="Q&Aを探す">
-                <button type="submit">検索</button>
-            </form>
-            <div class="icon">
-                <img src="img/icon.png" height="80" weight="100">
-            </div>
-        </div>
-    </header>
-    <div class="question">
-    <a class="questionn" href="question.php" >aaaaaaaaaa</a>
+<div class="aaa">
+    <?php
+    $pdo= new PDO($connect,USER,PASS);
+    $sql=$pdo->query('select * from user');
+    $row = $sql->fetch(PDO::FETCH_ASSOC);
+    echo '<div class="user">';
+    echo '<img src="img/icon.png" height="80" width="100">';
+    echo '<div class="u_name">',$row['name'],'　さん';
+    echo '<div class="u_name">',$row['name'],'　さん','<br>';
+    echo '</div>';
+    echo '</div>';
+    echo '<div class="profile">';
+    echo '<div class="u_name">',$row['name'],'　さん','<br>';
+    ?>
+    <div class="bbb">
+        
     </div>
-    
+</div>
 
-    <div class="flex">
-
-    <div class="aaa"></div>
-    
-
-    </div>
-        <script src="js/profile.js"></script> <!-- JavaScriptファイルの読み込み -->
+</div>
 </body>
 
 </html>
