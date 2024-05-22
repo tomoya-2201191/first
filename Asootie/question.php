6db6919a50b563b0b2bd8495380fc720b2683e7b
tomoya-2201191 <148730789+tomoya-2201191@users.noreply.github.com>
2024-05-22 10:18:40 +0900

    up

--- Asootie/question.php
+++ Asootie/question.php
@@ -1,7 +1,9 @@
 <?php
-session_start();;
+session_start();
 require 'db-connect.php';
 require 'header.php';
+
+// '共感した'ボタンが押されたときの処理
 if (isset($_POST['kyokan'])) {
     $q_id = $_POST['q_id'];
     $update_sql = 'UPDATE question SET feel = feel + 1 WHERE q_id = ?';
@@ -15,37 +17,37 @@ if (isset($_POST['kyokan'])) {
 
 <div class="left">
     <?php
-    $pdo= new PDO($connect,USER,PASS);
-    $sql=$pdo->query('select * from user');
+    $pdo = new PDO($connect, USER, PASS);
+    $sql = $pdo->query('select * from user');
     $row = $sql->fetch(PDO::FETCH_ASSOC);
     echo '<div class="q_user">';
     echo '<img src="img/icon.png" height="80" width="100">';
-    echo '<div class="q_profile">',$row['name'],'　さん','<br>';
-    if($row['status_id'] == 0){
+    echo '<div class="q_profile">', $row['name'], '　さん', '<br>';
+    if ($row['status_id'] == 0) {
         echo    '<div class="box1">
                 <div class="status1">STUDENT</div>
-                </div>'; // コメントの修正: 閉じタグを追加
-    }else if($row['status_id'] == 1){
+                </div>';
+    } else if ($row['status_id'] == 1) {
         echo    '<div class="box2">
                 <div class="status2">TEACHER</div>
-                </div>'; // コメントの修正: 閉じタグを追加
-    }else{
+                </div>';
+    } else {
         echo    '<div class="box3">
                 <div class="status3">GRADUATE</div>
-                </div>'; // コメントの修正: 閉じタグを追加
+                </div>';
     }
     echo '</div>'; // q_profile の終了タグを追加
-    $sql=$pdo->prepare('select * from question where q_id=?');
+    $sql = $pdo->prepare('select * from question where q_id=?');
     $sql->execute([$_GET['id']]);
     $row = $sql->fetch(PDO::FETCH_ASSOC);
-    echo '<div class="date">',$row['q_date'],'</div>';
-    echo '<div class="answer_sum">',$row['answer_sum'],'　回答','</div>';
+    echo '<div class="date">', $row['q_date'], '</div>';
+    echo '<div class="answer_sum">', $row['answer_sum'], '　回答', '</div>';
     echo '</div>'; // q_user の終了タグを修正
     $id = $_GET['id'];
-    echo '<div class="q_text">',$row['q_text'],'</div>';
-    echo '<form method="post" action="question.php">';
-    echo '<input type="hidden" name="q_id" value='.$id.'>';
-    echo '<button type="submit" name="kyokan" class="btn1">共感した '.$row['feel'].'</button></form>';
+    echo '<div class="q_text">', $row['q_text'], '</div>';
+    echo '<form method="post" action="">';
+    echo '<input type="hidden" name="q_id" value="', $id, '">';
+    echo '<button type="submit" name="kyokan" class="btn1">共感した ', $row['feel'], '</button></form>';
     echo '<button class="check_answer"><a href="view-answer.php?q_id=' . $id . '">回答を見る＞</a></button>';
     echo '<button class="q_answer"><a href="ranking.php?q_id=' . $id . '">回答をする＞</a></button>';
     ?>
@@ -54,13 +56,13 @@ if (isset($_POST['kyokan'])) {
 <div class="right">
     <?php
     echo '<div class="category">';
-    $sql=$pdo->query('select * from category');
-    echo '<br>','　カテゴリ一覧';
+    $sql = $pdo->query('select * from category');
+    echo '<br>　カテゴリ一覧';
     echo '<hr>';
     echo '<ul>';
     foreach ($sql as $row) {
-        $id=$row['category_id'];
-        echo '<li><a href="#?id=', $id, '">',$row['category_name'],"</li>";
+        $id = $row['category_id'];
+        echo '<li><a href="#?id=', $id, '">', $row['category_name'], "</li>";
         echo '<br>';
     }
     echo "</ul>";
@@ -69,7 +71,7 @@ if (isset($_POST['kyokan'])) {
 </div>
 
 </div>
-    <script src="js/top.js"></script> <!-- JavaScriptファイルの読み込み -->
+<script src="js/top.js"></script> <!-- JavaScriptファイルの読み込み -->
 </body>
 
 </html>
--- Asootie/view-answer.php
+++ Asootie/view-answer.php
@@ -19,34 +19,38 @@ require 'header.php';
         INNER JOIN user ON answer.a_user_id = user.user_id 
         WHERE answer.q_id = ?
     ');
-    $sql->execute([$_GET['q_id']]);
-    $id = NULL;
-    foreach($sql as $row) {
-        $id = $row['q_id'];
-        echo '<div class="q_user">';
-        echo '<img src="img/icon.png" height="80" width="110">';
-        echo '<div class="q_profile">', $row['name'], ' さん<br>';
-        if ($row['status_id'] == 0) {
-            echo '<div class="box1">
-                    <div class="status1">STUDENT</div>
-                  </div>';
-        } elseif ($row['status_id'] == 1) {
-            echo '<div class="box2">
-                    <div class="status2">TEACHER</div>
-                  </div>';
-        } else {
-            echo '<div class="box3">
-                    <div class="status3">GRADUATE</div>
-                  </div>';
+    $id = $_GET['q_id'];
+    $sql->execute([$id]);
+    $results = $sql->fetchAll();
+    if (count($results) === 0) {
+        echo '<div class="no-answer"><h3>回答はありません</h3></div>';
+    } else {
+        foreach ($results as $row) {
+            echo '<div class="q_user">';
+            echo '<img src="img/icon.png" height="80" width="110">';
+            echo '<div class="q_profile">', $row['name'], ' さん<br>';
+            if ($row['status_id'] == 0) {
+                echo '<div class="box1">
+                        <div class="status1">STUDENT</div>
+                      </div>';
+            } elseif ($row['status_id'] == 1) {
+                echo '<div class="box2">
+                        <div class="status2">TEACHER</div>
+                      </div>';
+            } else {
+                echo '<div class="box3">
+                        <div class="status3">GRADUATE</div>
+                      </div>';
+            }
+            echo '</div>'; // q_profile の終了タグを追加
+            echo '<div class="date">', $row['a_date'], '</div><br>';
+            if ($row['ba_flag'] == 1) {
+                echo '<div class="best-img"><img src="img/bestanswer.png" height="120" width="150"></div>';
+            }
+            echo '</div>'; // q_user の終了タグを追加
+            echo '<div class="a_text"><p>', $row['a_text'], '</p></div>';
+            echo '<hr>';
         }
-        echo '</div>'; // q_profile の終了タグを追加
-        echo '<div class="date">', $row['a_date'], '</div><br>';
-        if ($row['ba_flag'] == 1) {
-            echo '<div class="best-img"><img src="img/bestanswer.png" height="120" width="150"></div>';
-        }
-        echo '</div>'; // q_user の終了タグを追加
-        echo '<div class="a_text"><p>',$row['a_text'],'</p></div>';
-        echo '<hr>';
     }
     echo '<button class="back"><a href="question.php?id=' . $id . '">戻る＞</a></button>';
     ?>
