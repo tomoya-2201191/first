43bf79139bcceed27e5df36aeab770f13d11f841
tomoya-2201191 <148730789+tomoya-2201191@users.noreply.github.com>
2024-05-24 11:24:11 +0900

    update

---
+++ Asootie/admin-login.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require 'db-connect.php';
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>管理者ログイン</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/admin-style.css">
+    <!--link rel="stylesheet" href="css/style.css"> -->
+    <!-- <link rel="stylesheet" href="css/fade.css">  -->
+</head>
+<body>
+    <div class="back-white">
+        <div class="admin-img">
+            <img src="img/king-logo.png" width="150" height="150">
+        </div>
+        <form action="#" method="post">
+            <input type="text" class="textbox" name="email" placeholder="Email Address" required>
+            <div class="under"></div>
+            <input type="text" class="textbox" name="password" placeholder="Password" required>
+            <div class="under"></div>
+            <button type="submit" class="login-btn">ログイン</button>
+        </form>
+    </div>
+</body>
+<?php
+require 'footer.php';
+?>
