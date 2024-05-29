c9d6a9b21f80361ad873dcedc13758d9a6798a12
kyo0622 <148731274+kyo0622@users.noreply.github.com>
2024-05-29 10:57:37 +0900

    up

--- Asootie/profile.php
+++ Asootie/profile.php
@@ -4,33 +4,63 @@ require 'db-connect.php';
 require 'header.php';
 
 ?>
-<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/profile.css">
-<div class="a1"></div>
 
-<div class="flex">
-
-<div class="aaa">
     <?php
     $pdo= new PDO($connect,USER,PASS);
     $sql=$pdo->query('select * from user');
     $row = $sql->fetch(PDO::FETCH_ASSOC);
+    //ユーザー//
     echo '<div class="user">';
     echo '<img src="img/icon.png" height="80" width="100">';
-    echo '<div class="u_name">',$row['name'],'　さん';
-    echo '<div class="u_name">',$row['name'],'　さん','<br>';
-    echo '</div>';
-    echo '</div>';
-    echo '<div class="profile">';
-    echo '<div class="u_name">',$row['name'],'　さん','<br>';
-    ?>
-    <div class="bbb">
-        
-    </div>
-</div>
+    echo '<div class="u_name">',$row['name'],'　さん','<br>','</div>';
+    echo '<div class="u_master">';
 
-</div>
+    if ($row['best_answer'] < 5) {
+        echo    '<div class="box1">
+                <div class="status1">新人</div>
+                </div>';
+    } elseif ($row['best_answer'] < 10) {
+        echo    '<div class="box1">
+                <div class="status1">STUDENT</div>
+                </div>';
+    } elseif ($row['best_answer'] < 20) {
+        echo    '<div class="box1">
+                <div class="status1">STUDENT</div>
+                </div>';
+    } else {
+        echo    '<div class="box1">
+                <div class="status1">STUDENT</div>
+                </div>';
+    }
+    echo '</div>';
+    //プロフィール//
+    echo '<div class="profile">';
+    echo '<div class="p_gender">',$row['gender'],'<br>','</div>';
+    echo '<div class="p_status">',$row['status_id'],'<br>','</div>';
+    if ($row['status_id'] == 0) {
+        echo    '<div class="box1">
+                <div class="status1">STUDENT</div>
+                </div>';
+    } else if ($row['status_id'] == 1) {
+        echo    '<div class="box2">
+                <div class="status2">TEACHER</div>
+                </div>';
+    } else {
+        echo    '<div class="box3">
+                <div class="status3">GRADUATE</div>
+                </div>';
+    }
+    echo '<div class="p_coin">',$row['coin'],'<br>','</div>';
+    echo '</div>';
+    //レコード//
+    echo '<div class="record">';
+    echo '<div class="r_upload">',$row['upload'],'<br>','</div>';
+    echo '<div class="p_solution">',$row['solution'],'<br>','</div>';
+    echo '<div class="p_best">',$row['best_answer'],'<br>','</div>';
+    echo '<div class="p_other">',$row['other'],'<br>','</div>';
+    echo '</div>';
+    echo '</div>';
+    ?>
 </body>
 
 </html>
