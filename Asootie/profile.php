24fccef903c28daa96cef800e3dd4db898f27178
kyo0622 <148731274+kyo0622@users.noreply.github.com>
2024-06-12 10:06:45 +0900

    up

--- Asootie/profile.php
+++ Asootie/profile.php
@@ -8,7 +8,8 @@ require 'header.php';
 <div class="waku">
     <?php
     $pdo= new PDO($connect,USER,PASS);
-    $sql=$pdo->query('select * from user');
+    $sql = $pdo->prepare('select * from user where user_id=?');
+    $sql->execute([$_GET['user_id']]);
     $row = $sql->fetch(PDO::FETCH_ASSOC);
     //ユーザー//
     echo '<div class="user">';
@@ -18,19 +19,19 @@ require 'header.php';
 
     if ($row['best_answer'] < 5) {
         echo    '<div class="box1">
-                <div class="status1">新人</div>
+                <div class="status1">就活素人</div>
                 </div>';
     } elseif ($row['best_answer'] < 10) {
-        echo    '<div class="box1">
-                <div class="status1">STUDENT</div>
+        echo    '<div class="box2">
+                <div class="status2">就活中級者</div>
                 </div>';
     } elseif ($row['best_answer'] < 20) {
-        echo    '<div class="box1">
-                <div class="status1">STUDENT</div>
+        echo    '<div class="box3">
+                <div class="status3">就活マスター</div>
                 </div>';
     } else {
-        echo    '<div class="box1">
-                <div class="status1">STUDENT</div>
+        echo    '<div class="box3">
+                <div class="status3">STUDENT</div>
                 </div>';
     }
     echo '</div>';
@@ -39,6 +40,7 @@ require 'header.php';
     echo '<div class="profile">';
     echo '<div class="t_gender">','性別','<br>','</div>';//テキスト//
     echo '<div class="p_gender">',$row['gender'],'<br>','</div>';
+    echo '<hr>';
     echo '<div class="t_status">','職業','<br>','</div>';//テキスト//
     echo '<div class="p_status">';
     if ($row['status_id'] == 0) {
@@ -49,27 +51,34 @@ require 'header.php';
         echo    '卒業生';
     }
     echo '</div>';
-    echo '<div class="t_coin">','就活コイン','<br>','</div>';//テキスト//
-    echo '<img src="img/coin.png" height="40" width="50">';
+    echo '<hr>';
+    echo '<div class="t_coin">','就活コイン','</div>';//テキスト//
+    echo '<div class="t_coin_img">','<img src="img/coin.png" height="50" width="50">','</div>';
     echo '<div class="p_coin">',$row['coin'],'<br>','</div>';
     echo '</div>';
     //レコード//
     echo '<div class="record">';
     echo '<div class="t_upload">','総質問数','<br>','</div>';//テキスト//
     echo '<div class="r_upload">',$row['upload'],'<br>','</div>';
+    echo '<hr>';
     echo '<div class="t_solution">','解決質問数','<br>','</div>';//テキスト//
     echo '<div class="r_solution">',$row['solution'],'<br>','</div>';
+    echo '<hr>';
     echo '<div class="t_best">','ベストアンサー数','<br>','</div>';//テキスト//
     echo '<div class="r_best">',$row['best_answer'],'<br>','</div>';
+    echo '<hr>';
     echo '<div class="t_other">','その他の回答数','<br>','</div>';//テキスト//
     echo '<div class="r_other">',$row['other'],'<br>','</div>';
     echo '</div>';
+    $id = $_GET['user_id'];
     //ボタン//
-    echo '<button class="question_answer"><a class="a_color" href="#">質問＆回答一覧＞</a></button>';
-    echo '<button class="user_up"><a class="a_color" href="#">個人情報更新＞</a></button>';
+    echo '<button class="question_answer"><a class="a_color" href="question&answer.php?q&a_id=' . $id . '">質問＆回答一覧＞</a></button>';
+    echo '<button class="user_up"><a class="a_color" href="customer-update-input.php?id=' . $id . '">個人情報更新＞</a></button>';
     ?>
 </div>
-
+<?php
+echo '<button class="logout"><a class="a_color" href="logout.php">ログアウト＞</a></button>';
+?>
 <script src="js/top.js"></script>
 </body>
 
