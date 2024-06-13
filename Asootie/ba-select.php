bf828ccf03b18b4e84aec30102b28d00990443c2
tomoya-2201191 <148730789+tomoya-2201191@users.noreply.github.com>
2024-06-13 12:43:31 +0900

    update

---
+++ Asootie/ba-select.php
@@ -0,0 +1,125 @@
+<?php
+session_start();;
+require 'db-connect.php';
+require 'header.php';
+
+?>
+<div class="contents"><p>ベストアンサー選択</p></div>
+
+<div class="flex">
+
+<div class="left">
+    <?php
+
+    
+    // ベストアンサーフラッグを更新
+    $updateAnswer = $pdo->prepare('
+    UPDATE answer 
+    SET ba_flag = 1 
+    WHERE a_id = ?
+    ');
+    $updateSuccess1 = $updateAnswer->execute([$_POST['a_id']]);
+
+    //解決済みフラッグを更新
+    $updateQuestion = $pdo->prepare('
+    UPDATE question 
+    SET flag = 1 
+    WHERE q_id = ?
+    ');
+    $updateSuccess2 = $updateQuestion->execute([$_POST['q_id']]);
+
+    // 質問情報の取得
+    $question = $pdo->prepare('SELECT * FROM question WHERE q_id = ?');
+    $question->execute([$_POST['q_id']]);
+    $questionRow = $question->fetch(PDO::FETCH_ASSOC); // fetchAll()をfetch()に変更
+    $getCoin = $questionRow['coin'];
+
+    $user = $pdo->prepare('SELECT * FROM user WHERE user_id = ?');
+    $user->execute([$_POST['a_user_id']]);
+    $userRow = $user->fetch(PDO::FETCH_ASSOC); // fetchAll()をfetch()に変更
+    $updateCoin = $userRow['coin'] + $getCoin;
+
+    $updateUser = $pdo->prepare('
+        UPDATE user
+        INNER JOIN answer ON user.user_id = answer.a_user_id
+        SET user.coin = ?, user.best_answer = user.best_answer + 1, user.other = user.other - 1
+        WHERE answer.a_id = ?
+    ');
+    $updateSuccess3 = $updateUser->execute([$updateCoin, $_POST['a_id']]);
+
+    $sql = $pdo->prepare('
+        SELECT answer.*, user.*
+        FROM answer 
+        INNER JOIN user ON answer.a_user_id = user.user_id 
+        WHERE answer.q_id = ?
+    ');
+    $id = $_POST['q_id'];
+    $sql->execute([$id]);
+    $results = $sql->fetchAll();
+    echo '<div class="no-answer"><h3>ベストアンサーに選択しました！</h3></div><hr>';
+        foreach ($results as $row) {
+            echo '<div class="a_user">';
+            echo '<img src="img/icon.png" height="80" width="110">';
+            echo '<div class="q_profile">', $row['name'], ' さん<br>';
+            if ($row['status_id'] == 0) {
+                echo '<div class="box1">
+                        <div class="status1">STUDENT</div>
+                      </div>';
+            } elseif ($row['status_id'] == 1) {
+                echo '<div class="box2">
+                        <div class="status2">TEACHER</div>
+                      </div>';
+            } else {
+                echo '<div class="box3">
+                        <div class="status3">GRADUATE</div>
+                      </div>';
+            }
+            echo '</div>'; // q_profile の終了タグを追加
+            echo '<div class="date">', $row['a_date'], '</div><br>';
+            echo '</div>'; // q_user の終了タグを追加
+            if ($row['ba_flag'] == 1) {
+                echo '<div class="best-img"><img src="img/bestanswer.png" height="140" width="170">';
+                echo '<div class="best_text"><p>', $row['a_text'], '</p></div></div>';
+            }else{
+                echo '<div class="a_text"><p>', $row['a_text'], '</p></div>';
+            }
+            echo '<form method="post" action="">';
+            echo '<input type="hidden" name="a_id" value="', $row['a_id'], '">';
+            echo '<button type="submit" name="sankou" class="sankou">参考になる ', $row['reference'], '</button></form><hr>';
+
+            /*$sql = $pdo->prepare('
+            SELECT *
+            FROM question 
+            WHERE q_id = ?
+        ');
+        $sql->execute([$id]);
+        $question = $sql->fetch(PDO::FETCH_ASSOC);*/
+        }
+    //echo '<button class="back"><a class="modoru-color" href="question.php?id=' . $id . '">＜戻る</a></button>';
+    ?>
+</div>
+
+
+<div class="right">
+    <?php
+    echo '<div class="category">';
+    $sql=$pdo->query('select * from category');
+    echo '<br>','　カテゴリ一覧';
+    echo '<hr>';
+    echo '<ul>';
+    foreach ($sql as $row) {
+        $id=$row['category_id'];
+        echo '<li><a class="category-black" href="?id=', $id, '">', $row['category_name'], "</a></li>";
+        echo '<br>';
+    }
+    echo "</ul>";
+    echo '<hr>';
+    
+    ?>
+</div>
+
+</div>
+    <script src="js/top.js"></script> <!-- JavaScriptファイルの読み込み -->
+</body>
+
+</html>
