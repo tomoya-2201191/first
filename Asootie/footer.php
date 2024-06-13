049d90a9bcb290e73763e23d810944731362d739
Myon　 <2201195@s.asojuku.ac.jp>
2024-06-13 12:45:05 +0900

    up

--- Asootie/footer.php
+++ Asootie/footer.php
@@ -3,6 +3,7 @@
 <script src="js/top.js"></script> 
     <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
     <script src="js/5-6.js"></script>
+    </div><!--wrapper -->
 </body>
 
 </html>
--- Asootie/top.php
+++ Asootie/top.php
@@ -2,12 +2,11 @@
 session_start();
 require 'db-connect.php';
 
-
 // カテゴリIDの設定
 $category_id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 
 // フィルターの設定
-$filter = isset($_GET['filter']) ? $_GET['filter'] : 'all';
+$filter = isset($_GET['filter']) ? $_GET['filter'] : 'open';  // デフォルトは 'open'
 
 // カテゴリ名の取得
 $category_name = 'Q&A一覧';
@@ -60,97 +59,95 @@ foreach ($sql_params as $key => $value) {
 }
 $sql->execute();
 
-
-
- require 'header.php';
+require 'header.php';
 ?>
 
+<div class="contents">
+    <p><?php echo htmlspecialchars($category_name); ?></p>
+</div>
 
-
-        <div class="contents">
-            <p><?php echo htmlspecialchars($category_name); ?></p>
-        </div>
-
-        <div class="flex">
-            <div class="left">
-                <div class="left-1">
-                    <div class="left-1-1">
-                        <a href="?filter=open&id=<?php echo $category_id; ?>">
-                            <h3>回答受付中</h3>
-                        </a>
-                    </div>
-                    <div class="left-1-2">
-                        <a href="?filter=closed&id=<?php echo $category_id; ?>">
-                            <h3>解決済み</h3>
-                        </a>
-                    </div>
-                    <div class="left-1-2">
-                        <a href="?filter=all&id=<?php echo $category_id; ?>">
-                            <h3>すべて</h3>
-                        </a>
-                    </div>
-                </div>
-
-                <?php
-                echo '<div class="top-question">';
-                echo '<ul>';
-                foreach ($sql as $row) {
-                    $category = $row['category_name'];
-                    $id = $row['q_id'];
-                    $text = $row['q_text'];
-                    $answer = $row['answer_sum'];
-                    $date = $row['q_date'];
-
-                    // 文字数を制限して語尾に[...]を追加
-                    if (mb_strlen($text) > 38) {
-                        $text = mb_substr($text, 0, 38) . '...';
-                    }
-                    echo '<div class="top-category">', htmlspecialchars($category), '</div>';
-                    echo '<a class="top-text" href="question.php?id=', $id, '">', htmlspecialchars($text), '</a>';
-
-                    echo '<div class="flex">';
-                    echo '<div class="top-answer-date">';
-                    echo  '💬', htmlspecialchars($answer), "　";
-                    echo  htmlspecialchars($date);
-                    echo '</div>';
-                    echo '</div>';
-
-                    echo "<hr>";
-                    echo '<br>';
-                }
-                echo "</ul>";
-                echo "</div>";
-
-                // ページャーの表示
-                echo '<div class="pager">';
-                for ($i = 1; $i <= $total_pages; $i++) {
-                    echo '<a href="?filter=', $filter, '&page=', $i, '&id=', $category_id, '">', $i, '</a> ';
-                }
-                echo '</div>';
-                ?>
+<div class="flex">
+    <div class="left">
+        <div class="left-1">
+            <div class="left-1-1">
+                <a href="?filter=open&id=<?php echo $category_id; ?>" class="<?php echo $filter == 'open' ? 'selected' : ''; ?>">
+                    <h3>回答受付中</h3>
+                </a>
             </div>
-
-            <div class="right">
-                <?php
-                echo '<div class="category">';
-                $sql = $pdo->query('SELECT * FROM category');
-                echo '<br>', '　カテゴリ一覧';
-                echo '<hr>';
-                echo '<ul>';
-                foreach ($sql as $row) {
-                    $id = $row['category_id'];
-                    echo '<li><a class="category-black" href="?id=', $id, '">', htmlspecialchars($row['category_name']), "</a></li>";
-                    echo '<br>';
-                }
-                echo "</ul>";
-                echo '<hr>';
-                ?>
+            <div class="left-1-2">
+                <a href="?filter=closed&id=<?php echo $category_id; ?>" class="<?php echo $filter == 'closed' ? 'selected' : ''; ?>">
+                    <h3>解決済み</h3>
+                </a>
+            </div>
+            <div class="left-1-3">
+                <a href="?filter=all&id=<?php echo $category_id; ?>" class="<?php echo $filter == 'all' ? 'selected' : ''; ?>">
+                    <h3>すべて</h3>
+                </a>
             </div>
         </div>
+
+        <?php
+        echo '<div class="top-question">';
+        echo '<ul>';
+        foreach ($sql as $row) {
+            $category = $row['category_name'];
+            $id = $row['q_id'];
+            $text = $row['q_text'];
+            $answer = $row['answer_sum'];
+            $date = $row['q_date'];
+            $flag = $row['flag']; // 0: open, 1: closed
+
+            // 文字数を制限して語尾に[...]を追加
+            if (mb_strlen($text) > 38) {
+                $text = mb_substr($text, 0, 38) . '...';
+            }
+
+            // ステータスクラスの設定
+            $status_class = $flag == 0 ? 'status-open' : 'status-closed';
+
+            echo '<div class="top-category">', htmlspecialchars($category), '</div>';
+            echo '<a class="top-text" href="question.php?id=', $id, '">', htmlspecialchars($text), '</a>';
+
+            echo '<div class="flex">';
+            echo '<div class="top-answer-date ', $status_class, '">';
+            echo  '💬', htmlspecialchars($answer), "　";
+            echo  htmlspecialchars($date);
+            echo '</div>';
+            echo '</div>';
+
+            echo "<hr>";
+            echo '<br>';
+        }
+        echo "</ul>";
+        echo "</div>";
+
+        // ページャーの表示
+        echo '<div class="pager">';
+        for ($i = 1; $i <= $total_pages; $i++) {
+            echo '<a href="?filter=', $filter, '&page=', $i, '&id=', $category_id, '">', $i, '</a> ';
+        }
+        echo '</div>';
+        ?>
     </div>
 
-    <?php
-    require 'footer.php';
-    ?>
-</div><!--wrapper -->
+    <div class="right">
+        <?php
+        echo '<div class="category">';
+        $sql = $pdo->query('SELECT * FROM category');
+        echo '<br>', '　カテゴリ一覧';
+        echo '<hr>';
+        echo '<ul>';
+        foreach ($sql as $row) {
+            $id = $row['category_id'];
+            echo '<li><a class="category-black" href="?id=', $id, '">', htmlspecialchars($row['category_name']), "</a></li>";
+            echo '<br>';
+        }
+        echo "</ul>";
+        echo '<hr>';
+        ?>
+    </div>
+</div>
 
+<?php
+require 'footer.php';
+?>
