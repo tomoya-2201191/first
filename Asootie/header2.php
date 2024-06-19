d2240387313258a5baf6fa13102361217c828462
anjiexex <148730738+anjiexex@users.noreply.github.com>
2024-06-19 12:02:05 +0900

    up

--- Asootie/header2.php
+++ Asootie/header2.php
@@ -1,6 +1,12 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+require 'db-connect.php';
+?>
 <!DOCTYPE html>
 <html lang="ja">
- 
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,12 +18,10 @@
     <link rel="stylesheet" href="css/answer.css">
     <!-- <link rel="stylesheet" href="css/fade.css">  -->
 </head>
- 
+
 <body>
     <div class="header-top3"></div>
- 
- 
- 
+
     <div class="area">
         <ul class="circles">
             <li></li>
@@ -32,18 +36,14 @@
             <li></li>
         </ul>
     </div>
- 
- 
- 
+
     <header>
- 
         <div class="logo">
             <a href="top.php">
                 <img src="img/asootie.png" alt="ロゴ">
             </a>
         </div>
         <!-- <div class="search_box">
- 
             <form method="get" action="#" class="search">
                 <div class="searchForm">
                     <input type="text" class="searchForm-input" placeholder="Q&Aを探す">
@@ -51,28 +51,31 @@
                 </div>
             </form>
         </div> -->
- 
- 
+
         <form action="#" class="search-form-1">
             <label>
                 <input type="text" placeholder="キーワードを入力">
             </label>
             <button type="submit" aria-label="検索"></button>
         </form>
- 
- 
- 
+
         <div class="icon">
-            <a href="#">
-                <img src="img/icon.png">
-            </a>
+            <?php
+            if (isset($_SESSION['user_id'])) {
+                echo '<a href="user-profile.php?user_id=' . $_SESSION['user_id'] . '">';
+                echo '<img src="img/icon.png" alt="ユーザーアイコン">';
+                echo '</a>';
+            } else {
+                echo '<a href="login.php">';
+                echo '<img src="img/icon.png" alt="ユーザーアイコン">';
+                echo '</a>';
+            }
+            ?>
         </div>
- 
- 
     </header>
- 
- 
- 
+
     <div class="question">
         <a class="questionn" href="situmontoukou.php">質問・相談はこちら</a>
-</div>
+    </div>
+</body>
+</html>
